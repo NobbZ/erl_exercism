6162fc42a83fd04730eab985b1fd90d2caf622c7
Norbert Melzer <timmelzer@gmail.com>
2017-11-09 23:11:46 +0100

    try to find the module

--- src/exercism_parse_transform.erl
+++ src/exercism_parse_transform.erl
@@ -9,22 +9,57 @@
 
 parse_transform(Form, _Option) ->
     {ok, TestModuleName} = find_module_name(Form),
+    {ok, ModuleName} = extract_tested_module(TestModuleName),
     io:format("ModuleName: ~p~n~n~p~n", [TestModuleName, Form]),
-    Form.
+
+    Form1 = case is_example() of
+        false ->
+            Form;
+        true ->
+            replace_foreign_calls(Form, ModuleName)
+    end,
+
+    io:format("~p~n", [Form1]),
+    Form1.
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
 
-sut(Module) ->
+is_example() ->
     {ok, Files} = file:list_dir("./src"),
-    SUT = case lists:member("example.erl", Files) of
-        true  -> example;
-        false -> Module
-    end,
-    io:format("Subject under test: ~p~n", [SUT]),
-    SUT.
+    lists:member("exaple.erl", Files).
 
 find_module_name([]) -> {error, notfound};
 find_module_name([{attribute,_Line,module,Module}|_]) -> {ok, Module};
 find_module_name([_|T]) -> find_module_name(T).
+
+extract_tested_module(ModuleName) when is_atom(ModuleName) ->
+    ModuleName1 = atom_to_list(ModuleName),
+    case re:run(ModuleName1, "^(.*)_tests$") of
+        {match, [_, {Start, Length}]} ->
+            ModuleName2 = string:slice(ModuleName1, Start, Length),
+            {ok, list_to_atom(ModuleName2)};
+        _ ->
+            error
+    end.
+
+replace_foreign_calls(F, _) -> F.
+
+%%====================================================================
+%% Internal testing
+%%====================================================================
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+-define(_assertExtract(Exp, Module), ?_assertMatch(Exp, extract_tested_module(Module))).
+
+extract_tested_module_test_() ->
+    [?_assertExtract({ok, hello_world}, hello_world_tests)
+    ,?_assertExtract({ok, zipper},      zipper_tests)
+    ,?_assertExtract(error,             hello_world)
+    ,?_assertExtract(error,             zipper)
+    ].
+
+-endif.
